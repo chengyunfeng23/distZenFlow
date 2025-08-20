de51115e8454b62d0f9c7945aeb59fa66c80e32b
chengyunfeng <1476466395@qq.com>
2025-08-20 16:18:09 +0800

    c

--- src/views/home.ts
+++ src/views/home.ts
@@ -14,19 +14,19 @@ export default [
     {
         id:'DefaultComponents',
         name:'默认组件',
-        code:`/public/vue/DefaultComponents.vue`,
+        code:`/dist/vue/DefaultComponents.vue`,
         component:DefaultComponents
     },
     {
         id:'CustomizeDragAndDropNodesOnTheRight',
         name:'自定义右边拖拽节点',
-        code:`/public/vue/CustomizeDragAndDropNodesOnTheRight.vue`,
+        code:`/dist/vue/CustomizeDragAndDropNodesOnTheRight.vue`,
         component:CustomizeDragAndDropNodesOnTheRight
     },
     {
         id:'Html',
         name:'自定义画布的节点样式',
-        code:`/public/vue/Html.vue`,
+        code:`/dist/vue/Html.vue`,
         component:Html
     }
 ]
