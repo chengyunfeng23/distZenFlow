f86ae8552e3cfb76002429d794745f8580af185f
chengyunfeng <1476466395@qq.com>
2025-08-20 14:41:01 +0800

    c

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base:'./',
-   publicPath: './',
+  base:'./',
   plugins: [vue(), vueJsx(), vueDevTools()],
   rollupOptions: {
     output: {
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+// vue.config.js
+module.exports = {
+    publicPath: './'  // 关键配置，使用相对路径
+  }
