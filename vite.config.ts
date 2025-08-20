d608cfa1b504048fb04d246b43457ea0ef8ff503
chengyunfeng <1476466395@qq.com>
2025-08-20 14:35:23 +0800

    c

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  // base:'./',
+   publicPath: './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   rollupOptions: {
     output: {
